b789c2f601bf8e61849ecb69b0bcae3508a5a17f
Larissa Novaes <larissanovaes00@outlook.com>
2026-02-16 19:45:13 -0300

    adicionando componente de alerta

---
+++ src/app/services/alert/alert.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+
+export type AlertType = "success" | "danger" | "warning" | "info";
+
+@Injectable({ providedIn: "root" })
+export class AlertService {
+  private alertSubject = new BehaviorSubject<{
+    message: string;
+    type: AlertType;
+  } | null>(null);
+
+  alert$ = this.alertSubject.asObservable();
+
+  success(message: string) {
+    this.emit(message, "success");
+  }
+
+  error(message: string) {
+    this.emit(message, "danger");
+  }
+
+  warning(message: string) {
+    this.emit(message, "warning");
+  }
+
+  info(message: string) {
+    this.emit(message, "info");
+  }
+
+  clear() {
+    this.alertSubject.next(null);
+  }
+
+  private emit(message: string, type: AlertType) {
+    this.alertSubject.next({ message, type });
+    setTimeout(() => this.clear(), 3000);
+  }
+}
