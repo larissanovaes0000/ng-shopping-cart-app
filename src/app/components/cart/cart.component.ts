484bfec071e249a8f8faec46035d67fafc78a16a
Larissa Novaes <larissanovaes00@outlook.com>
2026-02-18 14:32:20 -0300

    fix cart and sort behaviour on click outside

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef } from "@angular/core";
 import { CartService } from "@services/cart/cart.service";
 import { ASSETS } from "app/shared/constants/assets.constants";
+import { ClickOutsideListener } from "app/shared/utils/click-outside-listener";
 import { map } from "rxjs/internal/operators/map";
 
 @Component({
@@ -8,7 +9,7 @@ import { map } from "rxjs/internal/operators/map";
   templateUrl: "./cart.component.html",
   styleUrls: ["./cart.component.scss"],
 })
-export class CartComponent {
+export class CartComponent extends ClickOutsideListener {
   openCartInformation = false;
   cartItems$ = this.cartService.getCartItems();
   totalQuantity$ = this.cartService.getTotalQuantity();
@@ -16,9 +17,11 @@ export class CartComponent {
   emptyCart$ = this.cartItems$.pipe(map((items: any[]) => items.length === 0));
 
   cartIcon = ASSETS.CART_ICON;
-  downIcon = ASSETS.DOWN_ICON
+  downIcon = ASSETS.DOWN_ICON;
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, elementRef: ElementRef<HTMLElement>) {
+    super(elementRef);
+  }
 
   clearCart() {
     this.cartService.clearCart();
@@ -26,6 +29,14 @@ export class CartComponent {
 
   handleCartInformation() {
     this.openCartInformation = !this.openCartInformation;
+  }
+
+  protected get isOpened(): boolean {
+    return this.openCartInformation;
+  }
+
+  protected closeOnOutsideClick(): void {
+    this.openCartInformation = false;
   }
 
   removeProduct(productId: number) {
